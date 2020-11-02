d3cf87236b4944e13b0d73ff33ac3e471ac79310
Ola Kalinowska <ola@u.northwestern.edu>
2020-11-01 19:28:14 -0600

    data analysis

---
+++ Fall2020-DataAnalysis/ScoringAnalysis/data_proc_bib.py
@@ -0,0 +1,112 @@
+from numpy import genfromtxt
+import numpy as np
+import csv
+
+################################################################################
+# This program reads the NIH prelim data for all subjects, computes various
+# metrics for each trial, and saves them to csv files: one for each subject
+# within their file and one overall data sheet with all subjects. The csv files
+# are formatted to be easily read by R for statistcal analysis,
+################################################################################
+
+#set directory where data is mounted
+#DIR = "/media/ola/Data/Research/DowntownData/Fall2020LabData/" # linux
+DIR = "E:\Research\DowntownData\Fall2020LabData\\" # windows
+
+number_of_control_subjects = 6
+number_of_stroke_subjects = 0
+DT = 0.05
+
+list_of_participants = ["S02","S03","S04","S05","S06","S07"]
+list_of_participants_num = ["2","3","4","5","6","7"]
+side_arm = ["R","R","L","L","R","L"]
+dom_arm = ["D","N","D","N","D","N"]
+#impairment = []
+# for i in range(0,number_of_stroke_subjects):
+#     impairment.append("Stroke")
+# for i in range(number_of_stroke_subjects, number_of_stroke_subjects+number_of_control_subjects):
+#     impairment.append("Control")
+
+################################################################################
+#               Define functions for generating metrics                        #
+################################################################################
+
+def calc_metrics(trialfile):
+    """
+    modified time to completion add 1 sec for every flag not collected
+    """
+    data = np.genfromtxt(trialfile,delimiter=',') #,dtype=float)
+    data = np.delete(data,0,0) # Deletes the first column of column names
+    score = data[-1,7] # Gets the last value of the score
+    tinbowl = 0.
+    for i in range(len(data[:,0])):
+        if data[i,3]>-0.14:
+            if data[i,6]<0.021:
+                tinbowl+=DT
+    #tinbowlpercent = tinbowltocompletion / 30.0
+    #meanvel,maxvel,meanvellifted = calc_ballvel(data[:,7], tlifted_vec) #4:6 #if just 6, calculates the velocity in the z direction
+    #meanjerk,maxjerk = calc_jerk(data[delayInd:,1:3], tlifted_vec) #1:3 #if just 1:2, calculates jerk in the xy plane
+
+    meanenergy = np.mean(data[:,16])
+    #meanenergy = calc_ball_energy(data[:,4:7], len(data[:,0]))
+    #print([score,tinbowl,meanenergy])
+    return [score,tinbowl,meanenergy] # meanvel,maxvel,meanvellifted,meanjerk,maxjerk,intenergy,windowenergy]
+
+def calc_ball_energy(pos, trial_len):
+    vel = np.gradient(pos,axis=0)
+    ballenergy_vec = np.zeros(trial_len, trial_len)
+    for i in range(trial_len):
+        ballenergy_vec[i] = 9.81*pos[i,2]+0.5*pow(vel[i,0],2)+0.5*pow(vel[i,1],2)+0.5*pow(vel[i,2],2)
+    # ballenergyDER_vec = np.gradient(ballenergy_vec,axis=0)
+    # ballenergyDER_lifted = np.multiply(tlifted_vec, ballenergyDER_vec)
+    # windowenergy = np.max(windowing(ballenergy_vec,100))
+    # intenergy = np.sum(ballenergy_lifted)
+    meanenergy = np.nanmean(np.where(ballenergy_vec!=0,ballenergy_vec,np.nan))
+    # medianenergy = np.nanmedian(np.where(ballenergy_lifted!=0,ballenergy_lifted,np.nan))
+    return [meanenergy] #,intenergy,windowenergy]
+
+def agg_data(subject_num, sub, subfile, file_all, csvfile_all, testwriter_all):
+    # Label factors as strings for ezANOVA analysis
+    # supportlevels = ['0%Max','20%Max','50%Max']
+    freq = ['Freq0','Freq1','Freq2','Freq3']
+
+    # Loop through each file, compute metrics, and save to file/s
+    for j in range(0,4): #frequency
+        for k in range(1,49): #trials
+            for i in range(0,2): #forces
+                try:
+                    #print(DIR+sub+subfile+"_Trial"+str(k)+"_Freq"+str(j)+"_SL0"+"_F"+str(i)+".csv")
+                    row = calc_metrics(DIR+sub+subfile+"_Trial"+str(k)+"_Freq"+str(j)+"_SL0_F"+str(i)+".csv")
+                    #print(row)
+                    # inserts the trial information before metrics
+                    row.insert(0,j) # frequency
+                    row.insert(0,i) # forces
+                    row.insert(0,k) # trial num
+                    row.insert(0,list_of_participants[subject_num])
+                    row.insert(0,dom_arm[subject_num])
+                    row.insert(0,side_arm[subject_num])
+                    with open (file_all,'a', newline="") as csvfile_all:
+                        testwriter_all = csv.writer(csvfile_all,delimiter=',')
+                        testwriter_all.writerow(row)
+                except:
+                    pass
+
+
+################################################################################
+#         Generate an "all-metrics-comparison" file for comparison analysis    #
+################################################################################
+
+file_all = DIR+"all-metrics-bib.csv"
+columns_all = ["Side","Dominance","Subject","TrialNum","Forces","Freq","Score","TIB","MeanEnergy"]
+with open(file_all,'w', newline="") as csvfile_all:
+    testwriter_all = csv.writer(csvfile_all,delimiter=',')
+    testwriter_all.writerow(columns_all)
+
+################################################################################
+#                 Loop through all subjects (stroke & control)                 #
+################################################################################
+for subject_num in range(0,len(list_of_participants)):
+    sub = list_of_participants[subject_num]
+    subfile = "\OutputData_S" + list_of_participants_num[subject_num]
+    print(list_of_participants[subject_num])
+    agg_data(subject_num, sub, subfile, file_all, csvfile_all, testwriter_all)
---
+++ Fall2020-DataAnalysis/ScoringAnalysis/labmates_stats_bib.r
@@ -0,0 +1,87 @@
+
+
+options(contrasts=c("contr.sum","contr.poly"))
+remove(list = ls())
+library(ez) # loads the ez package and dependencies
+
+col_default = c('steelblue','tomato3')
+
+DIR = "/media/ola/Data/Research/DowntownData/Fall2020LabData/"
+
+data = read.csv(paste(DIR,paste("all","metrics","comparison.csv",sep="-"),sep="/"))
+sink(paste(DIR,paste("labmates","comparison.txt",sep="-"),sep="/"))
+
+type_participants = "labmates"
+freq_labels = c("Freq1","Freq2","Freq3","Freq4")
+
+attach(data)
+
+#################################################
+cat("\n")
+cat("############ Score ############ \n")
+# mod.ez<-ezANOVA(data,ModScore,Subject,within = .(SupportLevel,Impairment),between = NULL, detailed = TRUE)
+# print(mod.ez)
+mscoremodel<-aov(Score~(Freq*Forces) + Error(Subject/(Freq*Forces)))
+print(summary(mscoremodel))
+
+mscoremodel<-aov(Score~(Dominance*Side) + Error(Subject))
+print(summary(mscoremodel))
+
+jpeg(paste(DIR,'Score_forces.jpg',sep="/"))
+    boxplot(Score~Forces,main = type_participants,xlab = "Forces",ylab="Score")
+dev.off()
+jpeg(paste(DIR,'Score_freqs.jpg',sep="/"))
+    boxplot(Score~Freq,main = type_participants,xlab = "Freqs",ylab="Score")
+           #col=col_default,ylim=c(0,40),names=supportlevel_labels)
+    #legend("bottomleft",c("controls","stroke"),cex=1.2,fill=col_default)
+dev.off()
+jpeg(paste(DIR,'Score_forces&freqs.jpg',sep="/"))
+    boxplot(Score~Forces*Freq,main = type_participants,xlab = "Forces*Freq",ylab="Score")
+dev.off()
+
+#################################################
+cat("\n")
+cat("############ Time ball-in-bowl ############ \n")
+#mod.ez<-ezANOVA(data,TIB,Subject,within = .(SupportLevel,Impairment),between = NULL, detailed = TRUE)
+#print(mod.ez)
+mscoremodel<-aov(TIB~(Freq*Forces) + Error(Subject/(Freq*Forces)))
+print(summary(mscoremodel))
+
+mscoremodel<-aov(TIB~(Dominance*Side) + Error(Subject))
+print(summary(mscoremodel))
+
+jpeg(paste(DIR,'TIB_forces.jpg',sep="/"))
+    boxplot(TIB~Forces,main = type_participants,xlab = "Forces",ylab="Time Ball-in-bowl")
+dev.off()
+jpeg(paste(DIR,'TIB_freqs.jpg',sep="/"))
+    boxplot(TIB~Freq,main = type_participants,xlab = "Freqs",ylab="Time Ball-in-bowl")
+           #col=col_default,ylim=c(0.5,1),names=supportlevel_labels)
+    #legend("bottomleft",c("controls","stroke"),cex=1.2,fill=col_default)
+dev.off()
+jpeg(paste(DIR,'TIB_forces&freqs.jpg',sep="/"))
+    boxplot(TIB~Forces*Freq,main = type_participants,xlab = "Forces*Freq",ylab="Time Ball-in-bowl",ylim=c(5,30))
+dev.off()
+
+
+#################################################
+cat("\n")
+cat("############ Mean Ball Energy ############ \n")
+#mod.ez<-ezANOVA(data,TIB,Subject,within = .(SupportLevel,Impairment),between = NULL, detailed = TRUE)
+#print(mod.ez)
+mscoremodel<-aov(MeanEnergy~(Freq*Forces) + Error(Subject/(Freq*Forces)))
+print(summary(mscoremodel))
+
+mscoremodel<-aov(MeanEnergy~(Dominance*Side) + Error(Subject))
+print(summary(mscoremodel))
+
+jpeg(paste(DIR,'MeanEnergy_forces.jpg',sep="/"))
+    boxplot(MeanEnergy~Forces,main = type_participants,xlab = "Forces",ylab="Mean Energy",ylim=c(0,10))
+           #col=col_default,ylim=c(0.5,1),names=supportlevel_labels)
+    #legend("bottomleft",c("controls","stroke"),cex=1.2,fill=col_default)
+dev.off()
+jpeg(paste(DIR,'MeanEnergy_freq.jpg',sep="/"))
+    boxplot(MeanEnergy~Freq,main = type_participants,xlab = "Freqs",ylab="Mean Energy",ylim=c(0,10))
+dev.off()
+jpeg(paste(DIR,'MeanEnergy_forces&freqs.jpg',sep="/"))
+    boxplot(MeanEnergy~Forces*Freq,main = type_participants,xlab = "Forces*Freqs",ylab="Mean Energy",ylim=c(0,10))
+dev.off()
