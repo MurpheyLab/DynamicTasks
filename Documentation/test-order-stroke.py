98e34a8902a1e065b5a53ae7ac750ce5bbb11eda
Milli <milli@u.northwestern.edu>
2021-03-19 13:58:23 -0500

    switch arms test order

--- Documentation/test-order-stroke.py
+++ Documentation/test-order-stroke.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import random
 
-minsub = 1
+minsub = 3
 maxsub = 8
 
 for sub in range(minsub,maxsub+1):
@@ -17,6 +17,7 @@ for sub in range(minsub,maxsub+1):
         num_conditions = num_support*num_freq #number of experimental conditions with
         num_repetitions = 5
         num_arms = 2
+        num_sets = 2
         arms_list = ['Nonparetic','Paretic']
         trialnum = 1
 
@@ -29,45 +30,61 @@ for sub in range(minsub,maxsub+1):
 
         arm_first = random.randint(0,num_arms-1)
         for day in range(2):
-            for arm in range(num_arms):
-                if arm==0:
-                    if day==1:
-                        arm_first = not(arm_first)
-                    arm_i = arm_first
-                    if day==0:
-                        testwriter.writerow([arms_list[not(arm_i)],'30%max (SL1)'])
-                        testwriter.writerow([arms_list[not(arm_i)],'0%max (SL0)'])
-                    else:
-                        testwriter.writerow([])
-                        testwriter.writerow([])
-                        testwriter.writerow(['DAY 2'])
-                    testwriter.writerow([arms_list[not(arm_i)],'practice ball-in-bowl'])
-                    if day==0:
-                        testwriter.writerow([arms_list[arm_i],'30%max (SL1)'])
-                        testwriter.writerow([arms_list[arm_i],'0%max (SL0)'])
-                    testwriter.writerow([arms_list[arm_i],'practice ball-in-bowl'])
-                    testwriter.writerow(['DAY '+str(day+1)+'---BALL-IN-BOWL'])
-                    columns = ['Arm','Supportlevel','Frequency','trial']
-                    testwriter.writerow(columns)
-                elif arm==1:
-                    arm_i = not(arm_first)
-                    testwriter.writerow(['Mandatory 5min break + switch arms'])
 
-                # set experimental order
-                trial_order = np.arange(0,num_conditions,1)
-                random.shuffle(trial_order)
+            # set experimental order
+            trial_order_0 = np.arange(0,num_conditions,1)
+            trial_order_1 = np.arange(0,num_conditions,1)
+            random.shuffle(trial_order_0)
+            random.shuffle(trial_order_1)
 
-                for trial in range(num_conditions):
-                    if trial == 4:
-                        testwriter.writerow(['Mandatory 5min break'])
-                    trial_int = trial_order[trial]
-                    for trial_count in range(0,num_repetitions):
-                        row=[arms_list[arm_i],SL_options[trial_int],F_options[trial_int],trialnum]
-                        testwriter.writerow(row)
-                        trialnum+=1
+            for set in range(num_sets):
+                for arm in range(num_arms):
+                    if arm==0:
+                        if day==1 and set==0:
+                            arm_first = not(arm_first)
+                        arm_i = arm_first
+                        if day==0 and set==0:
+                            testwriter.writerow([arms_list[not(arm_i)],'30%max (SL1)'])
+                            testwriter.writerow([arms_list[not(arm_i)],'0%max (SL0)'])
+                        elif day==1 and set==0:
+                            testwriter.writerow([])
+                            testwriter.writerow([])
+                            testwriter.writerow(['DAY 2'])
+                        if set==0:
+                            testwriter.writerow([arms_list[not(arm_i)],'practice ball-in-bowl'])
+                        if day==0 and set==0:
+                            testwriter.writerow([arms_list[arm_i],'30%max (SL1)'])
+                            testwriter.writerow([arms_list[arm_i],'0%max (SL0)'])
+                        if set==0:
+                            testwriter.writerow([arms_list[arm_i],'practice ball-in-bowl'])
+                            testwriter.writerow(['DAY '+str(day+1)+'---BALL-IN-BOWL'])
+                        testwriter.writerow(['Set '+str(set+1)])
+                        if set==1:
+                            testwriter.writerow(['Mandatory 5min break + switch arms'])
+
+                        columns = ['Arm','Supportlevel','Frequency','trial']
+                        testwriter.writerow(columns)
+                    elif arm==1:
+                        arm_i = not(arm_first)
+                        testwriter.writerow(['Mandatory 5min break + switch arms'])
+
+
+                    for trial in range(set*4,set*4+4):
+                        # print(trial_order_0,trial,trial_order_0[trial])
+                        # if trial == 4:
+                        #     testwriter.writerow(['Mandatory 5min break'])
+                        if arm_i==arm_first:
+                            trial_int = trial_order_0[trial]
+                        else:
+                            trial_int = trial_order_1[trial]
+                        for trial_count in range(0,num_repetitions):
+                            row=[arms_list[arm_i],SL_options[trial_int]+1,F_options[trial_int],trialnum]
+                            testwriter.writerow(row)
+                            trialnum+=1
 
         # NAIL AND HAMMER
         trialnum = 1
+        num_repetitions = 6
         for arm in range(num_arms):
 
             if arm==0:
